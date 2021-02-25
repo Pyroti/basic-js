48ac478c226d53157320cbb3bfdd42808ff018bf
Pyroti <pavel.gangin@gmail.com>
2021-02-26 01:04:37 +0300

    fix: carbon-dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -4,6 +4,8 @@ const MODERN_ACTIVITY= 15;
 const HALF_LIFE_PERIOD= 5730;
 
 module.exports = function dateSample(sampleActivity) {
-  if(typeof +sampleActivity !== Number) return false;
-  return Math.ceil(Math.log(MODERN_ACTIVITY/sampleActivity)) / (0.693/HALF_LIFE_PERIOD);
+  if (typeof sampleActivity !== 'string' || sampleActivity > 15 || sampleActivity <= 0 || isNaN(sampleActivity)) {
+    return false;
+  }
+  return Math.ceil(Math.log(MODERN_ACTIVITY/sampleActivity) / (0.693/HALF_LIFE_PERIOD));
 };
